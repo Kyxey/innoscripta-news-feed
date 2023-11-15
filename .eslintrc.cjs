ba8f58ebbefe009161230fe5770770dcc8643ed2
KYXEY <maniazizjahan@gmail.com>
2023-11-15 09:03:15 +0000

    chore: configs eslint, prettier and installs the missing packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,22 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "react-hooks"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
-}
+  "settings": {
+    "import/resolver": {
+      "typescript": {}
+    }
+  }
+};
